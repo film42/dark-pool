ae06ce342fa9137cc0a3b0aea29359a262808363
Garrett Thornburg <film42@gmail.com>
2015-01-14 21:21:57 -0700

    add assertion

--- src/test/scala/RunnerSpec.scala
+++ src/test/scala/RunnerSpec.scala
@@ -3,7 +3,8 @@ import org.scalatest.FunSpec
 class RunnerSpec extends FunSpec {
   describe("Argument Checker") {
     it("can receive correct arguments") {
-      Runner.checkArguments(Array("-p 3000", "-c 10", "-m development"))
+      val status = Runner.checkArguments(Array("-p 3000", "-c 10", "-m development"))
+      assert(status)
     }
   }
 }
