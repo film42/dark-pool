c7e8341990b98c001588d889786de22ae051130e
Garrett Thornburg <film42@gmail.com>
2015-01-20 22:19:40 -0700

    create matching engine; add cancel order; move book to its own package

--- src/main/scala/darkpool/engine/MatchingEngine.scala
+++ src/main/scala/darkpool/engine/MatchingEngine.scala
@@ -1,30 +1,89 @@
 package darkpool.engine
 
-import java.util.UUID
-
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ActorSystem, Props}
 import darkpool.actors.LedgerActor
+import darkpool.book.OrderBook
 import darkpool.models.Trade
-import darkpool.models.book.OrderBook
 import darkpool.models.orders._
 
-class MatchingEngine(buyOrder: OrderBook[BuyOrder], sellOrder: OrderBook[SellOrder]) {
+class MatchingEngine(buyOrderBook: OrderBook[BuyOrder], sellOrderBook: OrderBook[SellOrder]) {
   val system = ActorSystem("dark-pool")
   val ledger = system.actorOf(Props[LedgerActor])
 
-  def insert[O <: Order](order: O): O = {
-    println(order)
-    order
+  private var _referencePrice: Option[Double] = None
+
+  def referencePrice = _referencePrice.get
+
+  def referencePrice_=(price: Double) {
+    _referencePrice = Some(price)
   }
 
-  def tryMatch(order: Order): Option[Order] = {
-
-    ledger ! Trade(UUID.randomUUID(), UUID.randomUUID(), 10, 10)
-
-    Some(order)
+  def getBooks(orderType: OrderType): (OrderBook[OrderType], OrderBook[OrderType]) = orderType match {
+    case _: BuyOrder => (buyOrderBook, sellOrderBook)
+    case _: SellOrder => (sellOrderBook, buyOrderBook)
   }
 
-  private def tryMatchWithTop(order: Order, top: Order): Option[Trade] = ???
+  def acceptOrder(order: Order) {
+    val (book, counterBook) = getBooks(order.orderType)
+    val unfilledOrder = tryMatch(order, counterBook)
+    unfilledOrder.map(book.add)
+  }
+
+  def tryMatch(order: Order, counterBook: OrderBook[OrderType]): Option[Order] = {
+    if (order.quantity == 0) None
+    else counterBook.top match {
+      case None => Some(order)
+      case Some(top) => tryMatchWithTop(order, top) match {
+        case None => Some(order)
+        case Some(trade) =>
+          counterBook.decreaseTopBy(trade.quantity)
+          ledger ! trade
+          val unfilledOrder = order.decreasedBy(trade.quantity)
+          tryMatch(unfilledOrder, counterBook)
+      }
+    }
+  }
+
+  // TODO: Do we need .orderType.isInstanceOf[BuyOrder] ???
+  // FIXME: This is nasty code
+  private def tryMatchWithTop(order: Order, top: Order): Option[Trade] = {
+    def trade(price: Double): Option[Trade] = {
+      _referencePrice = Some(price)
+      val (buy, sell) = if (order.orderType.isInstanceOf[BuyOrder]) (order, top) else (top, order)
+      Some(Trade(buy.id, sell.id, price, math.min(buy.quantity, sell.quantity)))
+    }
+
+    lazy val oppositeBestLimit: Option[Double] = {
+      val oppositeBook = if (order.orderType.isInstanceOf[BuyOrder]) sellOrderBook else buyOrderBook
+      oppositeBook.bestLimit
+    }
+
+    (order, top) match {
+      // There is a top limit order we can match anything with
+      case (_, topLimitOrder @ LimitOrder(_, _, _)) =>
+        if(order.crossesAt(topLimitOrder.threshold)) trade(topLimitOrder.threshold)
+        else None
+
+      // Match a limit order with a market order
+      case (limitOrder @ LimitOrder(_, _, _), MarketOrder(_, _)) =>
+        val limitThreshold = oppositeBestLimit match {
+          case Some(threshold) => if (order.crossesAt(threshold)) threshold else limitOrder.threshold
+          case None => limitOrder.threshold
+        }
+        trade(limitThreshold)
+
+      // Match a market order with a market order
+      case (MarketOrder(_, _), MarketOrder(_, _)) =>
+        val limitThreshold = oppositeBestLimit match {
+          case Some(threshold) => threshold
+          case None => _referencePrice match {
+            case Some(price) => price
+            case None => throw new IllegalStateException("Can't execute trade with two market orders without best limit or reference price")
+          }
+        }
+        trade(limitThreshold)
+    }
+  }
 
 }
 
--- src/main/scala/darkpool/models/orders.scala
+++ src/main/scala/darkpool/models/orders.scala
@@ -14,7 +14,13 @@ package object orders {
   trait SellOrder extends OrderType
   trait Order extends Quantity with CreatedAt with ID {
     def decreasedBy(quantity: Double): Order
-    def crossesAt(quantity: Double): Boolean
+    def crossesAt(price: Double): Boolean
+    // TODO: Rename this awful name
+    def orderType: OrderType = this match {
+      case order: BuyOrder => new BuyOrder {}
+      case order: SellOrder => new SellOrder {}
+      case _ => throw new IllegalStateException("Unknown OrderType")
+    }
   }
   trait Threshold {
     def threshold: Double
@@ -23,43 +29,55 @@ package object orders {
   //
   // Order Implementations
   //
-  case class MarketOrder(orderQuantity: Double, orderId: UUID)
+  case class MarketOrder[+Type](orderQuantity: Double, orderId: UUID)
     extends Order {
 
     override def id: UUID = orderId
     override def quantity: Double = orderQuantity
     // Market orders accept any price
-    override def crossesAt(quantity: Double): Boolean = true
-    override def decreasedBy(quantity: Double): MarketOrder =
-      MarketOrder(orderQuantity - quantity, orderId)
+    override def crossesAt(price: Double): Boolean = true
+    override def decreasedBy(quantity: Double): MarketOrder[Type] =
+      MarketOrder[Type](orderQuantity - quantity, orderId)
   }
 
-  case class LimitOrder(orderQuantity: Double, orderThreshold: Double, orderId: UUID)
+  case class LimitOrder[+Type](orderQuantity: Double, orderThreshold: Double, orderId: UUID)
     extends Order with Threshold {
 
     override def quantity: Double = orderQuantity
     override def threshold: Double = orderThreshold
     override def id: UUID = orderId
-    // FIXME: Add Buy/ Sell distinction here
-    override def crossesAt(quantity: Double): Boolean = {
-      false
+    override def crossesAt(price: Double): Boolean = this match {
+      case order: BuyOrder => price <= threshold
+      case order: SellOrder => price >= threshold
+      case _ => throw new IllegalStateException("Unknown OrderType")
     }
-    override def decreasedBy(quantity: Double): LimitOrder =
-      LimitOrder(orderQuantity - quantity, orderThreshold, orderId)
+    override def decreasedBy(quantity: Double): LimitOrder[Type] =
+      LimitOrder[Type](orderQuantity - quantity, orderThreshold, orderId)
   }
 
-  case class StopOrder(orderQuantity: Double, orderThreshold: Double, orderId: UUID)
+  case class StopOrder[+Type](orderQuantity: Double, orderThreshold: Double, orderId: UUID)
     extends Order with Threshold {
 
     override def quantity: Double = orderQuantity
     override def threshold: Double = orderThreshold
     override def id: UUID = orderId
-    // FIXME: Add Buy/ Sell distinction here
-    override def crossesAt(quantity: Double): Boolean = {
-      false
+    // FIXME: Check code
+    override def crossesAt(price: Double): Boolean = this match {
+      case order: BuyOrder => price <= threshold
+      case order: SellOrder => price >= threshold
+      case _ => throw new IllegalStateException("Unknown OrderType")
     }
-    override def decreasedBy(quantity: Double): StopOrder =
-      StopOrder(orderQuantity - quantity, orderThreshold, orderId)
+    override def decreasedBy(quantity: Double): StopOrder[Type] =
+      StopOrder[Type](orderQuantity - quantity, orderThreshold, orderId)
+  }
+
+  case class CancelOrder[+Type](orderId: UUID)
+    extends Order {
+
+    override def decreasedBy(quantity: Double): CancelOrder[Type] = CancelOrder[Type](orderId)
+    override def crossesAt(price: Double): Boolean = true
+    override def quantity: Double = 0.0
+    override def id: UUID = orderId
   }
 
 }
